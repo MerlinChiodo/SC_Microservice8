f223566f690e9adff5fedd6ee2656f0c9871b3dd
mblomenkamp <47089917+marioblm@users.noreply.github.com>
2022-05-22 11:30:08 +0200

    Add basic frontend for forms, blog and front page

---
+++ frontend/src/views/blog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h2>Aktuelles</h2>
+    <br />
+    <BlogCard
+      :description="description"
+      :title="title"
+      :target="target"
+      :image="image"
+      :date="date"
+    />
+    <br />
+  </div>
+</template>
+
+<script>
+import BlogCard from "../components/blogEntry.vue";
+export default {
+  name: "Blog",
+  data() {
+    return {
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At vitae laboriosam dolore debitis blanditiis delectus accusantium dicta, impedit sit asperiores quas veritatis fugit ea illum sed, alias reprehenderit itaque.",
+      title: "Entry 1",
+      target: "Rentner",
+      image:
+        "http://vps2290194.fastwebserver.de:9780/api/files/abc0d25a-550d-4359-b4a7-8cd35c287083.png",
+      date: "2021-01-01 10:00",
+    };
+  },
+  components: {
+    BlogCard,
+  },
+  methods: {},
+};
+</script>
