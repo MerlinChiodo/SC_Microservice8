1c25fc7a1513be7519d800c779d1bc57a4a8810b
mblomenkamp <47089917+marioblm@users.noreply.github.com>
2022-06-07 08:07:08 +0200

    Add backend for deadlines

--- backend/routes/internal/deadline.js
+++ backend/routes/internal/deadline.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { header, body, param } = require('express-validator');
-const { createDeadline, getDeadline, getAllDeadlines, deleteDeadline } = require("../../controllers/internal/deadline.js");
+const { createDeadline, getAllDeadlines, deleteDeadline } = require("../../controllers/internal/deadline.js");
 const router = express.Router()
 
 router.use(express.urlencoded({ extended: false }))
@@ -8,16 +8,12 @@ router.use(express.json())
 
 router.post('/',
     body('date').optional().isLength({min:11}).isISO8601(),
-    body('title').not().isEmpty().isAlphanumeric(),
-    body('description').not().isEmpty().isAlphanumeric(),
+    body('title').not().isEmpty(),
+    body('description').not().isEmpty(),
     header('token').not().isEmpty().trim().escape(),
     createDeadline)
 
 router.get('/all',getAllDeadlines)
-
-router.get('/:id',
-    param('id').not().isEmpty().isNumeric(),
-    getDeadline)
 
 router.delete('/:id',
     param('id').not().isEmpty().isNumeric(),
