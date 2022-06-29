4e257a4e13ea3f352405aea70fbab6f2dd13b9f7
mblomenkamp <47089917+marioblm@users.noreply.github.com>
2022-06-29 23:05:11 +0200

    Add overview over all processes for admins

---
+++ frontend/src/components/citizenCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h4 v-if="!citizens || citizens.length==0">Keine Bürger gefunden</h4>
+    <div v-else v-for="item in citizens" :key="item.id">
+        <div class="processWrapper" v-if="item.process.length>0">
+            <div class="col-4" >
+                <h4>Von: {{ item.lastname }}, {{ item.name }} - {{item.email}}</h4>
+                <div v-for="pitem in item.process" :key="pitem.id">
+                    <a :href="'vorgang/?id=' + pitem.id">Vorgang {{pitem.processTypes.name}}</a>
+                </div>
+                <a :href="'vorgaenge/' + item.id" class="btn btn-primary btn-lg" tabindex="-1" role="button">Übersicht</a>
+            </div>
+            <br>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bürger",
+  props: {
+    citizens: {
+      type: Array,
+      required: true,
+    }
+  },
+};
+</script>
--- frontend/src/views/internal/overview.vue
+++ frontend/src/views/internal/overview.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="container">
     <h3>Übersicht</h3>   
-    <p>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#contactRequests" aria-expanded="false" aria-controls="contactRequests">
-      Kontaktanfragen anzeigen
-    </button>
-  </p>
-  <div class="collapse" id="contactRequests">
-    <div class="card card-body">
-      <contactRequestCard :requests="contactRequests" />
+    <div class="d-grid gap-2">
+      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#contactRequests" aria-expanded="false" aria-controls="contactRequests">
+        Kontaktanfragen anzeigen
+      </button>
+      <div class="collapse" id="contactRequests">
+        <div class="card card-body">
+          <contactRequestCard :requests="contactRequests" />
+        </div>
+      </div> 
+      <br>
+      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#processRequests" aria-expanded="false" aria-controls="processRequests">
+        Vorgänge anzeigen
+      </button>
+      <div class="collapse" id="processRequests">
+        <div class="card card-body">
+          <citizenCard :citizens="citizens" />
+        </div>
+      </div> 
     </div>
-  </div> 
   </div>
 </template>
 
 <script>
 import contactRequestCard from "../../components/contactRequestCard.vue";
+import citizenCard from "../../components/citizenCard.vue";
 export default {
   name: "Vorgänge",
   data() {
     return {
-      contactRequests: []
+      contactRequests: [],
+      citizens: []
     };
   },
   mounted: function () {
@@ -28,18 +39,24 @@ export default {
       this.workerLogin();
     }else{
       this.getContactRequests();
+      this.getCitizens();
     }
   },
   components:{
-    contactRequestCard
+    contactRequestCard,
+    citizenCard
   },
   methods:{
     async getContactRequests(){
       let data = await this.fetch_get({} , "/api/contactRequest/all" );
       if(data){
         this.contactRequests = data.result;
-      }else{
-        this.$notify({group: "error",title: "Fehler!",text: "Keine Anfragen gefunden",},2000);
+      }
+    },
+    async getCitizens(){
+      let data = await this.fetch_get({} , "/api/citizen/all" );
+      if(data){
+        this.citizens = data.citizens;
       }
     }
   }
